d193fc587090dcca03782b0b5658eda3ff63a294
Luna Aphelion <luna-aphelion@outlook.com>
2022-08-12 21:19:11 +0100

    made vol-notif.sh work on dunstify instead of notify-send meaning dunst doesnt redraw the notification each time i change the volume

--- i3/scripts/vol-notif.sh
+++ i3/scripts/vol-notif.sh
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 
 #close all notifications with dunst, change this to an apropriate command if using a different notif daemon
-dunstctl close-all
+#dunstctl close-all
 
 volume=$(amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
 muted=$(amixer -D pulse get Master | grep '%' | head -n 1 | cut -d ']' -f 2 | cut -d '[' -f 2)
@@ -25,11 +25,14 @@ echo $muted
 bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
 if [[ $muted = "off" ]]; then
  #use the muted icon
- notify-send "muted" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
+ #notify-send "muted" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
+  dunstify -r 1 -I "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" "muted"
 elif [[ $volume > "20" ]]; then
  #use the high volume icon
- notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+ #notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+  dunstify -r 1 -I /usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg "$volume $bar"
 else
  #use the low volume icon
- notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+ #notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+  dunstify -r 1 -I "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg" "$volume $bar"
 fi
