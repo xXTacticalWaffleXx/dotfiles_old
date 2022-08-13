f95ba29c1ab57f5d9b8f788cdb9e0366d6829e37
Luna Aphelion <luna-aphelion@outlook.com>
2022-08-13 18:50:07 +0100

    removed the -w flag from the notify-send command (how was i that retarded again)

--- i3/scripts/vol-notif.sh
+++ i3/scripts/vol-notif.sh
@@ -25,14 +25,14 @@ echo $muted
 bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
 if [[ $muted = "off" ]]; then
  #use the muted icon
- notify-send "muted" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" -w -r 1
+ notify-send "muted" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" -r 1
  #dunstify -r 1 -I "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" "muted"
-elif [[ $volume >= "30" ]]; then
+elif [[ $volume -ge "30" ]]; then
  #use the high volume icon
- notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg" -w -r 1
+ notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg" -r 1
  #dunstify -r 1 -I /usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg "$volume $bar"
 else
  #use the low volume icon
- notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg" -w -r 1
+ notify-send "$volume  $bar" -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg" -r 1
  #dunstify -r 1 -I "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg" "$volume $bar"
 fi
